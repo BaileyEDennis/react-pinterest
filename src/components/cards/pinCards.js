60c4d1574eabc661065b711e564cd6aecf64e08c
Bailey <bailey.elise.dennis@gmail.com>
2020-11-21 00:35:53 -0600

    continue working on deleteting pin and associated pin-board

--- src/components/cards/pinCards.js
+++ src/components/cards/pinCards.js
@@ -8,9 +8,13 @@ export default function PinsCard({ pinData }) {
       <div className="card-body">
         <h5 className="card-title">{pinData.name}</h5>
         <p className="card-text">{pinData.description}</p>
+        <a className='btn btn-info' href={pinData.website}>Visit website</a>
         <Link className="btn btn-primary" to={`/pinData-edit/${pinData.firebaseKey}`}>
           Edit Pin
         </Link>
+        <button className="btn btn-danger" onClick={ () => { this.deleteAPin(pinData.firebaseKey); } }>
+          Delete Pin
+        </button>
       </div>
     </div>
   );
