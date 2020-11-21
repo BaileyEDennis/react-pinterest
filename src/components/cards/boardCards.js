ff7d4d44f32053150259442a1c2f9bad4658f218
Bailey <bailey.elise.dennis@gmail.com>
2020-11-21 00:00:15 -0600

    now able to delete boards

--- src/components/cards/boardCards.js
+++ src/components/cards/boardCards.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default class BoardCard extends Component {
   render() {
-    const { board } = this.props;
+    const { board, boardDataFunction } = this.props;
 
     return (
       <div className="Goat col-3" id={board.firebaseKey}>
@@ -13,6 +13,9 @@ export default class BoardCard extends Component {
             <h5 className="card-title">{board.name}</h5>
             <p className="card-text">{board.description}</p>
             <Link className='btn btn-primary' to={`/boards/${board.firebaseKey}`}>View Pins</Link>
+            <button onClick={ () => { boardDataFunction(board.firebaseKey); } }className='btn btn-dark delete-board board-buttons'>
+              <i className='far fa-trash-alt'></i> Delete Board
+            </button>
           </div>
         </div>
       </div>
